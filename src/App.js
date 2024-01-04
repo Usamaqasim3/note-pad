ee717586602631efc9edf038ea41ca89b3400a6e
usamaqasim3 <usama.qasim9999@gmail.com>
2024-01-05 00:06:24 +0500

    second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import Header from './Header'
+import Footer from './Footer'
+import CreateNote from './CreateNote'
+import Note from './Note'
 
-function App() {
+const App = () => {
+  const [addItem, setAddItem]= useState([]);
+  const addNote=(note)=>{
+    setAddItem((prevData)=>{
+        return [...prevData, note ]
+    })
+        
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <>
+
+    <Header/>
+    <CreateNote passNote={addNote}/>
+    
+    {addItem.map((val, index) =>{
+      return <Note
+        key={index}
+        id={index}
+        title={val.title}
+        content={val.content}
+      />
+    })}
+    <Footer/>
+    </>
+  )
 }
 
-export default App;
+export default App
---
+++ src/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import logo from './logo2.png'
+const Header = () => {
+  return (
+    <div className='header'>
+    
+    <img src={logo} alt="logo" />
+    <h1>Daily Notes</h1>
+    </div>
+  )
+}
+
+export default Header
